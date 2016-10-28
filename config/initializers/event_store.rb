550374bba23782edb470a480061a8a4f9a618a3d
James Brennan <james@jamesbrennan.ca>
2016-10-28 17:47:59 +0100

    Global event handler

--- config/initializers/event_store.rb
+++ config/initializers/event_store.rb
@@ -1,5 +1,8 @@
-EventStore = RailsEventStore::Client.new
-
 Rails.application.reloader.to_prepare do
+  # Clear all subscriptions when reloading
   EventStore = RailsEventStore::Client.new
+
+  EventStore.subscribe_to_all_events(
+    -> (event) { Rails.logger.info("#{event.class.to_s} published. Data: #{event.data.inspect}") }
+  )
 end
